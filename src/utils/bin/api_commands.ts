e9a3e2dfd24ba207da9b749cb0065e9eeed132ba
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-06-10 15:05:02 +0700

    reconfigure the app

--- src/utils/bin/api_commands.ts
+++ src/utils/bin/api_commands.ts
@@ -15,10 +15,10 @@ export const projects = async (args: string[]): Promise<string> => {
     .join('\n');
 };
 
-export const quote = async (args: string[]): Promise<string> => {
-  const data = await getQuote();
-  return data.quote;
-};
+// export const quote = async (args: string[]): Promise<string> => {
+//   const data = await getQuote();
+//   return data.quote;
+// };
 
 export const readme = async (args: string[]): Promise<string> => {
   const readme = await getReadme();
@@ -26,11 +26,11 @@ export const readme = async (args: string[]): Promise<string> => {
   ${readme}`;
 };
 
-export const weather = async (args: string[]): Promise<string> => {
-  const city = args.join('+');
-  if (!city) {
-    return 'Usage: weather [city]. Example: weather casablanca';
-  }
-  const weather = await getWeather(city);
-  return weather;
-};
+// export const weather = async (args: string[]): Promise<string> => {
+//   const city = args.join('+');
+//   if (!city) {
+//     return 'Usage: weather [city]. Example: weather casablanca';
+//   }
+//   const weather = await getWeather(city);
+//   return weather;
+// };
--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -8,11 +8,7 @@ export const help = async (args: string[]): Promise<string> => {
   const commands = Object.keys(bin).sort().join(', ');
   var c = '';
   for (let i = 1; i <= Object.keys(bin).sort().length; i++) {
-    if (i % 7 === 0) {
-      c += Object.keys(bin).sort()[i - 1] + '\n';
-    } else {
-      c += Object.keys(bin).sort()[i - 1] + ' ';
-    }
+    c += '> ' + Object.keys(bin).sort()[i - 1] + '\n';
   }
   return `Welcome! Here are all the available commands:
 \n${c}\n
@@ -30,7 +26,7 @@ export const repo = async (args: string[]): Promise<string> => {
 
 // About
 export const about = async (args: string[]): Promise<string> => {
-  return `Hi, I am ${config.name}. 
+  return `Hi, I am ${config.name}.
 Welcome to my website!
 More about me:
 'sumfetch' - short summary.
@@ -38,19 +34,19 @@ More about me:
 'readme' - my github readme.`;
 };
 
-export const resume = async (args: string[]): Promise<string> => {
-  window.open(`${config.resume_url}`);
-  return 'Opening resume...';
-};
+// export const resume = async (args: string[]): Promise<string> => {
+//   window.open(`${config.resume_url}`);
+//   return 'Opening resume...';
+// };
 
 // Donate
-export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
-`;
-};
+// export const donate = async (args: string[]): Promise<string> => {
+//   return `thank you for your interest.
+// here are the ways you can support my work:
+// - <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
+// - <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
+// `;
+// };
 
 // Contact
 export const email = async (args: string[]): Promise<string> => {
@@ -71,87 +67,119 @@ export const linkedin = async (args: string[]): Promise<string> => {
 };
 
 // Search
-export const google = async (args: string[]): Promise<string> => {
-  window.open(`https://google.com/search?q=${args.join(' ')}`);
-  return `Searching google for ${args.join(' ')}...`;
-};
+// export const google = async (args: string[]): Promise<string> => {
+//   window.open(`https://google.com/search?q=${args.join(' ')}`);
+//   return `Searching google for ${args.join(' ')}...`;
+// };
 
-export const duckduckgo = async (args: string[]): Promise<string> => {
-  window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
-  return `Searching duckduckgo for ${args.join(' ')}...`;
-};
+// export const duckduckgo = async (args: string[]): Promise<string> => {
+//   window.open(`https://duckduckgo.com/?q=${args.join(' ')}`);
+//   return `Searching duckduckgo for ${args.join(' ')}...`;
+// };
 
-export const bing = async (args: string[]): Promise<string> => {
-  window.open(`https://bing.com/search?q=${args.join(' ')}`);
-  return `Wow, really? You are using bing for ${args.join(' ')}?`;
-};
+// export const bing = async (args: string[]): Promise<string> => {
+//   window.open(`https://bing.com/search?q=${args.join(' ')}`);
+//   return `Wow, really? You are using bing for ${args.join(' ')}?`;
+// };
 
-export const reddit = async (args: string[]): Promise<string> => {
-  window.open(`https://www.reddit.com/search/?q=${args.join(' ')}`);
-  return `Searching reddit for ${args.join(' ')}...`;
-};
+// export const reddit = async (args: string[]): Promise<string> => {
+//   window.open(`https://www.reddit.com/search/?q=${args.join(' ')}`);
+//   return `Searching reddit for ${args.join(' ')}...`;
+// };
 
 // Typical linux commands
-export const echo = async (args: string[]): Promise<string> => {
-  return args.join(' ');
-};
+// export const echo = async (args: string[]): Promise<string> => {
+//   return args.join(' ');
+// };
 
-export const whoami = async (args: string[]): Promise<string> => {
-  return `${config.ps1_username}`;
-};
+// export const whoami = async (args: string[]): Promise<string> => {
+//   return `${config.ps1_username}`;
+// };
 
-export const ls = async (args: string[]): Promise<string> => {
-  return `a
-bunch
-of
-fake
-directories`;
-};
+// export const ls = async (args: string[]): Promise<string> => {
+//   return `a
+// bunch
+// of
+// fake
+// directories`;
+// };
 
-export const cd = async (args: string[]): Promise<string> => {
-  return `unfortunately, i cannot afford more directories.
-if you want to help, you can type 'donate'.`;
-};
+// export const cd = async (args: string[]): Promise<string> => {
+//   return `unfortunately, i cannot afford more directories.
+// if you want to help, you can type 'donate'.`;
+// };
 
-export const date = async (args: string[]): Promise<string> => {
-  return new Date().toString();
-};
+// export const date = async (args: string[]): Promise<string> => {
+//   return new Date().toString();
+// };
 
-export const vi = async (args: string[]): Promise<string> => {
-  return `woah, you still use 'vi'? just try 'vim'.`;
-};
+// export const vi = async (args: string[]): Promise<string> => {
+//   return `woah, you still use 'vi'? just try 'vim'.`;
+// };
 
-export const vim = async (args: string[]): Promise<string> => {
-  return `'vim' is so outdated. how about 'nvim'?`;
-};
+// export const vim = async (args: string[]): Promise<string> => {
+//   return `'vim' is so outdated. how about 'nvim'?`;
+// };
 
-export const nvim = async (args: string[]): Promise<string> => {
-  return `'nvim'? too fancy. why not 'emacs'?`;
-};
+// export const nvim = async (args: string[]): Promise<string> => {
+//   return `'nvim'? too fancy. why not 'emacs'?`;
+// };
 
-export const emacs = async (args?: string[]): Promise<string> => {
-  return `you know what? just use vscode.`;
-};
+// export const emacs = async (args?: string[]): Promise<string> => {
+//   return `you know what? just use vscode.`;
+// };
 
-export const sudo = async (args?: string[]): Promise<string> => {
-  window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
-  return `Permission denied: with little power comes... no responsibility? `;
-};
+// export const sudo = async (args?: string[]): Promise<string> => {
+//   window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ', '_blank'); // ...I'm sorry
+//   return `Permission denied: with little power comes... no responsibility? `;
+// };
 
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-█████        ███                       ███████████                                   
-░░███        ░░░                       ░█░░░███░░░█                                   
- ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
- ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
- ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
- ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
- ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
-░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
+  ██████╗  ██████╗     ██╗   ██╗ ██████╗ ██╗   ██╗    ██╗     ██╗██╗  ██╗███████╗
+  ██╔══██╗██╔═══██╗    ╚██╗ ██╔╝██╔═══██╗██║   ██║    ██║     ██║██║ ██╔╝██╔════╝
+  ██║  ██║██║   ██║     ╚████╔╝ ██║   ██║██║   ██║    ██║     ██║█████╔╝ █████╗
+  ██║  ██║██║   ██║      ╚██╔╝  ██║   ██║██║   ██║    ██║     ██║██╔═██╗ ██╔══╝
+  ██████╔╝╚██████╔╝       ██║   ╚██████╔╝╚██████╔╝    ███████╗██║██║  ██╗███████╗
+  ╚═════╝  ╚═════╝        ╚═╝    ╚═════╝  ╚═════╝     ╚══════╝╚═╝╚═╝  ╚═╝╚══════╝
+
+  ██████╗ ███████╗ █████╗  ██████╗████████╗    ██╗  ██╗ ██████╗  ██████╗ ██╗  ██╗███████╗██████╗
+  ██╔══██╗██╔════╝██╔══██╗██╔════╝╚══██╔══╝    ██║  ██║██╔═══██╗██╔═══██╗██║ ██╔╝██╔════╝╚════██╗
+  ██████╔╝█████╗  ███████║██║        ██║       ███████║██║   ██║██║   ██║█████╔╝ ███████╗  ▄███╔╝
+  ██╔══██╗██╔══╝  ██╔══██║██║        ██║       ██╔══██║██║   ██║██║   ██║██╔═██╗ ╚════██║  ▀▀══╝
+  ██║  ██║███████╗██║  ██║╚██████╗   ██║       ██║  ██║╚██████╔╝╚██████╔╝██║  ██╗███████║  ██╗
+  ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ ╚═════╝   ╚═╝       ╚═╝  ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝  ╚═╝
+
+  ███╗   ███╗███████╗    ████████╗ ██████╗  ██████╗
+  ████╗ ████║██╔════╝    ╚══██╔══╝██╔═══██╗██╔═══██╗
+  ██╔████╔██║█████╗         ██║   ██║   ██║██║   ██║
+  ██║╚██╔╝██║██╔══╝         ██║   ██║   ██║██║   ██║
+  ██║ ╚═╝ ██║███████╗       ██║   ╚██████╔╝╚██████╔╝▄█╗
+  ╚═╝     ╚═╝╚══════╝       ╚═╝    ╚═════╝  ╚═════╝ ╚═╝
+
+  ██╗     ███████╗████████╗███████╗    ████████╗███████╗ █████╗ ███╗   ███╗    ██╗   ██╗██████╗
+  ██║     ██╔════╝╚══██╔══╝██╔════╝    ╚══██╔══╝██╔════╝██╔══██╗████╗ ████║    ██║   ██║██╔══██╗
+  ██║     █████╗     ██║   ███████╗       ██║   █████╗  ███████║██╔████╔██║    ██║   ██║██████╔╝
+  ██║     ██╔══╝     ██║   ╚════██║       ██║   ██╔══╝  ██╔══██║██║╚██╔╝██║    ██║   ██║██╔═══╝
+  ███████╗███████╗   ██║   ███████║       ██║   ███████╗██║  ██║██║ ╚═╝ ██║    ╚██████╔╝██║██╗
+  ╚══════╝╚══════╝   ╚═╝   ╚══════╝       ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚═════╝ ╚═╝╚═╝
 
 Type 'help' to see the list of available commands.
-Type 'sumfetch' to display summary.
-Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
+Type 'sumfetch' to display summary.`;
+};
+
+export const credit = (args?: string[]): string => {
+  return `
+█████        ███                       ███████████
+░░███        ░░░                       ░█░░░███░░░█
+ ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████
+ ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███
+ ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███
+ ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███
+ ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
+░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░
+
+Created using LiveTerm. Check the repo here <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u>
 `;
 };
